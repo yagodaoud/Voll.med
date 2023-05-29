41f4f7a8e7e3dbca29108be297c4be4f880e4169
Yago Daoud <yagodaoud@hotmail.com>
2023-05-29 17:25:27 -0300

    Add Paciente methods

---
+++ src/main/java/com/br/med/voll/api/domain/paciente/DadosAtualizacaoPaciente.java
@@ -0,0 +1,12 @@
+package com.br.med.voll.api.domain.paciente;
+
+import com.br.med.voll.api.domain.endereco.DadosEndereco;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosAtualizacaoPaciente(
+        @NotNull
+        Long id,
+        String nome,
+        String telefone,
+        DadosEndereco endereco) {
+}
---
+++ src/main/java/com/br/med/voll/api/domain/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,10 @@
+package com.br.med.voll.api.domain.paciente;
+
+import com.br.med.voll.api.domain.endereco.Endereco;
+
+public record DadosDetalhamentoPaciente(Long id, String nome, String email, String cpf, String telefone, Endereco endereco) {
+
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf(), paciente.getTelefone(), paciente.getEndereco());
+    }
+}
